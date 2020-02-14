c85e78c066e417c7c51ae8fe0591995353c0f7aa
Jeffrey Luo <jl834@duke.edu>
2020-02-14 00:13:55 -0500

    Updated JavaScript to use ES6

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.querySelector(".gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector(".gradient");
 
-function updateGradient() {
+const updateGradient = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     css.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 }
